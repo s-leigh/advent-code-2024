2a9a5becf417590c176cd99790d051ee9d9396df
Stephen Leigh <stephen.leigh@fca.org.uk>
2024-12-02 18:54:20 +0000

    Day 2 tweak

--- src/main/kotlin/adventcode/day02/day02.kt
+++ src/main/kotlin/adventcode/day02/day02.kt
@@ -16,12 +16,12 @@ fun day02Part2(input: String): Int {
     return reports.count { it.isSafeWithTolerance() }
 }
 
-fun List<Int>.isSafe(): Boolean {
+private fun List<Int>.isSafe(): Boolean {
     val differences = this.zipWithNext().map(Pair<Int, Int>::difference)
     return (differences.all(Int::isNegative) || differences.all(Int::isPositive))
             && differences.all { d -> abs(d) in 1..3 }
 }
 
-fun List<Int>.isSafeWithTolerance() = this.indices
+private fun List<Int>.isSafeWithTolerance() = if (this.isSafe()) true else this.indices
     .map { toRemove -> this.filterIndexed { i, _ -> i != toRemove } }
     .any { it.isSafe() }
