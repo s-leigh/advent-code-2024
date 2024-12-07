a2f5144a4ce9f0ba997cee28d825dfe05d258da0
Stephen Leigh <s-leigh@users.noreply.github.com>
2024-12-07 15:21:29 +0000

    Efficiency

--- src/main/kotlin/adventcode/day07.kt
+++ src/main/kotlin/adventcode/day07.kt
@@ -1,55 +1,56 @@
 package adventcode
 
+import kotlin.math.log
+
 private data class Equation(val result: Long, val operands: List<Long>)
-private typealias Operation = (Long, Long) -> Long
+private data class Operation(val name: OperationName, val fn: (Long, Long) -> Long)
+private enum class OperationName {
+    MUL,
+    ADD,
+    CONCAT
+}
+
+private val operationAssociations: Set<Operation> = setOf(
+    Operation(OperationName.MUL, Long::div),
+    Operation(OperationName.ADD, Long::minus),
+    Operation(OperationName.CONCAT, Long::removeDigits)
+)
 
 fun day07Part1(input: String): Long {
     val data: List<Equation> = parseInput(input)
-    val availableOperations: Set<Operation> = setOf(Long::plus, Long::times)
-    return successfulEquationsSum(data, availableOperations)
+    val permittedOperationAssociations: Set<Operation> =
+        operationAssociations.filter { it.name == OperationName.MUL || it.name == OperationName.ADD }.toSet()
+    return data.filter { isSuccessful(it, permittedOperationAssociations) }.sumOf { it.result }
 }
 
 fun day07Part2(input: String): Long {
     val data: List<Equation> = parseInput(input)
-    val availableOperations: Set<Operation> = setOf(Long::plus, Long::times, Long::combine)
-    return successfulEquationsSum(data, availableOperations)
+    return data.filter { isSuccessful(it, operationAssociations) }.sumOf { it.result }
 }
 
 private fun parseInput(input: String): List<Equation> = input.asLinesSplitBy(":").map {
     Equation(it[0].toLong(), it[1].split(" ").filterNotEmpty().map(String::toLong))
 }
 
-private tailrec fun successfulEquationsSum(
-    equations: List<Equation>,
-    availableOperations: Set<Operation>,
-    successfulSum: Long = 0
-): Long {
-    if (equations.isEmpty()) return successfulSum
-    val (head, tail) = equations.headAndTail()
-    val ops = cartesianProduct(availableOperations, head.operands.size - 1)
-    ops.forEach { o ->
-        if (isSuccessful(head, o)) return successfulEquationsSum(tail, availableOperations, successfulSum + head.result)
+/* Go backwards through the operands, to determine which outcomes are impossible */
+private fun isSuccessful(eq: Equation, ops: Set<Operation>): Boolean {
+    if (eq.operands.size == 1) return eq.operands.single() == eq.result
+    if (eq.result == 1L) return false
+    return ops.any { op ->
+        if (op.name == OperationName.MUL && eq.result % eq.operands.last() != 0L) false
+        else if (op.name == OperationName.CONCAT && !eq.result.endsWith(eq.operands.last())) false
+        else isSuccessful(
+            Equation(
+                op.fn.invoke(eq.result, eq.operands.last()),
+                eq.operands.dropLast(1)
+            ), ops
+        )
     }
-    return successfulEquationsSum(tail, availableOperations, successfulSum)
 }
 
-private fun isSuccessful(equation: Equation, operations: List<Operation>): Boolean {
-    require(operations.size == equation.operands.size - 1) { "There must be the right number of operations for the equation." }
-    val res = equation.operands.reduceIndexed { i, acc, o ->
-        val res = operations[i - 1].invoke(acc, o)
-        if (res > equation.result) return false // can't possibly hit target
-        res
-    }
-    return res == equation.result
-}
+private fun Long.endsWith(other: Long): Boolean = this.toString().endsWith(other.toString())
 
-/* Combine two longs as strings, e.g. 123 + 456 = 123456 */
-private fun Long.combine(other: Long): Long {
-    var multiplier = 1L
-    var temp = other
-    while (temp > 0) {
-        multiplier *= 10
-        temp /= 10
-    }
-    return this * multiplier + other
-}
+/* Given a Long of N digits, remove the last N digits from this Long */
+private fun Long.removeDigits(other: Long): Long = this.toString().dropLast(
+    log(other.toFloat(), 10F).toInt() + 1
+).toLong()
