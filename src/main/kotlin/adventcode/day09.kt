5c67a59fb11ed2276927115713508f1d1cb02434
Stephen Leigh <s-leigh@users.noreply.github.com>
2024-12-09 20:41:28 +0000

    d9p2 wip

--- src/main/kotlin/adventcode/day09.kt
+++ src/main/kotlin/adventcode/day09.kt
@@ -12,11 +12,66 @@ fun day09Part1(input: String): Long {
         Block(blockId, n.toInt())
     }
     val blockElements: List<Block> = blocks.flatMap { Block(it.id, 1).repeat(it.size) }
-    val newDisk = blockElements.toMutableList().rearrange()
+    val newDisk = blockElements.toMutableList().rearrangeBlocks()
     return newDisk.checkSum()
 }
 
-private fun MutableList<Block>.rearrange(): List<Block> {
+fun day09Part2(input: String): Long {
+    val blocks = input.split("").filterNotEmpty().mapIndexed { i, n ->
+        val isFreeSpace = i % 2 == 1
+        val blockId = if (isFreeSpace) -1 else i / 2
+        Block(blockId, n.toInt())
+    }.filter { it.size > 0 }
+//    val blockElements: List<Block> = blocks.flatMap { Block(it.id, 1).repeat(it.size) }
+//    println("start: $blocks")
+    val newDisk = blocks.toMutableList().rearrangeFiles()
+//    println("end result: $newDisk")
+    return newDisk.checkSumFiles()
+}
+
+private fun MutableList<Block>.rearrangeFiles(): List<Block> {
+    for (i in this.lastIndex downTo 0) {
+        val b = this[i]
+        if (b.id < 0) continue
+        val nextEmptyI = this.nextEmpty(b.size)
+        if (nextEmptyI != null && nextEmptyI < i) {
+            this[i] = Block(-1, b.size)
+            val space = this[nextEmptyI]
+            val leftover = space.size - b.size
+            this.removeAt(nextEmptyI)
+            this.add(nextEmptyI, b)
+            if (leftover > 0) {
+//                if (nextEmptyI + 1 < this.lastIndex && this[nextEmptyI+1].id == -1)
+                this.add(nextEmptyI+1, Block(-1, leftover))
+//                println("leftovers: $this")
+            }
+//            this.combineEmpties()
+        }
+//        println(this)
+    }
+
+    return this
+}
+
+private fun MutableList<Block>.combineEmpties() {
+    val toBeRemoved = mutableListOf<Int>()
+    for (i in 1 .. this.lastIndex) {
+        val prev = this[i-1]
+        val curr = this[i]
+        if (prev.id != -1 || curr.id != -1) continue
+        this[i-1] = Block(-1, prev.size + curr.size)
+        toBeRemoved.add(i)
+    }
+    println("list: $this")
+    println("to remove: $toBeRemoved")
+    toBeRemoved.reversed().forEach{this.removeAt(it)}
+}
+
+private fun List<Block>.nextEmpty(minSize: Int) = this.mapIndexed{i, b -> if (b.id == -1 && b.size >= minSize) i else null}
+    .filterNotNull()
+    .firstOrNull()
+
+private fun MutableList<Block>.rearrangeBlocks(): List<Block> {
     val fillers: MutableList<Block> = this.filter { it.id >= 0 }.toMutableList()
     val numElements = fillers.size
     this.forEachIndexed { i, b ->
@@ -29,4 +84,8 @@ private fun MutableList<Block>.rearrange(): List<Block> {
     return this.subList(0, numElements)
 }
 
+private fun List<Block>.checkSumFiles(): Long = this.flatMap {
+//    println(Block(it.id, 1).repeat(it.size) )
+    Block(if (it.id == -1) 0 else it.id, 1).repeat(it.size)
+}.checkSum()
 private fun List<Block>.checkSum(): Long = this.mapIndexed { i, b -> i * b.id.toLong() }.sum()
--- src/test/kotlin/adventcode/Day09Test.kt
+++ src/test/kotlin/adventcode/Day09Test.kt
@@ -8,6 +8,7 @@ class Day09Test : StringSpec({
     val sampleInput1 = """2333133121414131402"""
     val sampleInput2 = "12345"
     val sampleInput3 = "1010101010101010101010"
+    val sampleInput4 = "354631466260"
 
     "Day 09 Part 1 sample input 1" {
         val expected = 1928L
@@ -29,13 +30,33 @@ class Day09Test : StringSpec({
         day09Part1(input) shouldBe expected
     }
 
-//    "Day 09 Part 2 sample input 1" {
-//        val expected = -1
-//        day09Part2(sampleInput1) shouldBe expected
-//    }
-//
-//    "Day 09 Part 2" {
-//        val expected = -1
-//        day09Part2(input) shouldBe expected
-//    }
+    "Day 09 Part 2 sample input 1" {
+        val expected = 2858L
+        day09Part2(sampleInput1) shouldBe expected
+    }
+
+    "Day 09 Part 2 sample input 2" {
+        val expected = 132L
+        day09Part2(sampleInput2) shouldBe expected
+    }
+
+    "Day 09 Part 2 sample input 3" {
+        val expected = 385L
+        day09Part2(sampleInput3) shouldBe expected
+    }
+
+    "Day 09 Part 2 sample input 4" {
+        val expected = 1325L
+        day09Part2(sampleInput4) shouldBe expected
+    }
+
+    "Day 09 Part 2 sample input x" {
+        val expected = 12L
+        day09Part2("111000000000000000001") shouldBe expected
+    }
+
+    "Day 09 Part 2" {
+        val expected = -1
+        day09Part2(input) shouldBe expected
+    }
 })
