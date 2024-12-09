6a9b422d6e52a0773c366916f39f601f0626aa58
Stephen Leigh <s-leigh@users.noreply.github.com>
2024-12-09 19:10:34 +0000

    Efficiency

--- src/main/kotlin/adventcode/day09.kt
+++ src/main/kotlin/adventcode/day09.kt
@@ -12,23 +12,21 @@ fun day09Part1(input: String): Long {
         Block(blockId, n.toInt())
     }
     val blockElements: List<Block> = blocks.flatMap { Block(it.id, 1).repeat(it.size) }
-    val newDisk = blockElements.rearrange()
+    val newDisk = blockElements.toMutableList().rearrange()
     return newDisk.checkSum()
 }
 
-private tailrec fun List<Block>.rearrange(
-    built: MutableList<Block> = mutableListOf<Block>(),
-    fillers: MutableList<Block> = this.filter { it.id >= 0 }.toMutableList()
-): List<Block> {
-    if (this.isEmpty() || fillers.isEmpty()) return built
-    val (head, tail) = this.headAndTail()
-    if (head.id < 0) {
-        built.add(fillers.last())
-        return tail.rearrange(built, fillers.dropLast(1).toMutableList())
-    } else {
-        built.add(this[0])
-        return tail.rearrange(built, fillers.drop(1).toMutableList())
+private fun MutableList<Block>.rearrange(): List<Block> {
+    val fillers: MutableList<Block> = this.filter { it.id >= 0 }.toMutableList()
+    val numElements = fillers.size
+    this.forEachIndexed { i, b ->
+        if (i > numElements) return@forEachIndexed
+        if (b.id == -1) {
+            this[i] = fillers.last()
+            fillers.removeLast()
+        }
     }
+    return this.subList(0, numElements)
 }
 
 private fun List<Block>.checkSum(): Long = this.mapIndexed { i, b -> i * b.id.toLong() }.sum()
