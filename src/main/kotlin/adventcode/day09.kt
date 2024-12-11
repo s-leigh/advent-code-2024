773af265b92a4abae3204af8e49249be3953eba3
Stephen Leigh <stephen.leigh@fca.org.uk>
2024-12-11 16:31:55 +0000

    Day 09 part 2

--- src/main/kotlin/adventcode/day09.kt
+++ src/main/kotlin/adventcode/day09.kt
@@ -1,14 +1,14 @@
 package adventcode
 
-private data class Block(val id: Int, val size: Int) {
-    override fun toString() = (if (this.id < 0) "." else this.id.toString()).repeat(this.size)
+private data class Block(val id: Int?, val size: Int) {
+    override fun toString() = (if (this.id == null) "." else this.id.toString()).repeat(this.size)
     fun repeat(times: Int): List<Block> = (0 until times).map { Block(this.id, 1) }
 }
 
 fun day09Part1(input: String): Long {
     val blocks = input.split("").filterNotEmpty().mapIndexed { i, n ->
         val isFreeSpace = i % 2 == 1
-        val blockId = if (isFreeSpace) -1 else i / 2
+        val blockId = if (isFreeSpace) null else i / 2
         Block(blockId, n.toInt())
     }
     val blockElements: List<Block> = blocks.flatMap { Block(it.id, 1).repeat(it.size) }
@@ -19,46 +19,38 @@ fun day09Part1(input: String): Long {
 fun day09Part2(input: String): Long {
     val blocks = input.split("").filterNotEmpty().mapIndexed { i, n ->
         val isFreeSpace = i % 2 == 1
-        val blockId = if (isFreeSpace) -1 else i / 2
+        val blockId = if (isFreeSpace) null else i / 2
         Block(blockId, n.toInt())
-    }.filter { it.size > 0 }
-//    println("start: $blocks")
-    val newDisk = blocks.toMutableList().rearrangeFiles()
-//    println("end result: $newDisk")
-    return newDisk.checkSum()
+    }
+    val res = rearrangeFiles(blocks.toMutableList())
+    val cs = res.checkSum()
+    return cs
 }
 
-private fun MutableList<Block>.rearrangeFiles(): List<Block> {
-    val new = this.toList().toMutableList()
-    for (i in new.lastIndex downTo 0) {
-        val b = new[i]
-        if (b.id < 0) continue
-        val nextEmptyI = new.nextEmpty(b.size)
-        if (nextEmptyI != null && nextEmptyI < i) {
-            new[i] = Block(-1, b.size)
-            val space = new[nextEmptyI]
-            val leftover = space.size - b.size
-            new.removeAt(nextEmptyI)
-            new.add(nextEmptyI, b)
-            if (leftover > 0) {
-                new.add(nextEmptyI+1, Block(-1, leftover))
-            }
+private fun rearrangeFiles(blocks: MutableList<Block>): List<Block> {
+    for (fileI in blocks.lastIndex downTo 0) {
+        val file = blocks[fileI]
+        if (file.id == null) continue
+        val freeSpaceI = blocks.firstFreeSpace(file.size)
+        if (freeSpaceI != null && freeSpaceI < fileI) {
+            val leftoverSpace = blocks[freeSpaceI].size - file.size
+            blocks[freeSpaceI] = file
+            blocks[fileI] = Block(null, file.size)
+            if (leftoverSpace > 0) blocks.add(freeSpaceI + 1, Block(null, leftoverSpace))
         }
     }
-
-    return new
+    return blocks
 }
 
-private fun List<Block>.nextEmpty(minSize: Int) = this.mapIndexed{i, b -> if (b.id == -1 && b.size >= minSize) i else null}
-    .filterNotNull()
-    .firstOrNull()
+private fun List<Block>.firstFreeSpace(minSize: Int): Int? =
+    this.mapIndexed { i, b -> if (b.id == null && b.size >= minSize) i else null }.filterNotNull().firstOrNull()
 
 private fun MutableList<Block>.rearrangeBlocks(): List<Block> {
-    val fillers: MutableList<Block> = this.filter { it.id >= 0 }.toMutableList()
+    val fillers: MutableList<Block> = this.filter { it.id != null }.toMutableList()
     val numElements = fillers.size
     this.forEachIndexed { i, b ->
         if (i > numElements) return@forEachIndexed
-        if (b.id == -1) {
+        if (b.id == null) {
             this[i] = fillers.last()
             fillers.removeLast()
         }
@@ -67,5 +59,5 @@ private fun MutableList<Block>.rearrangeBlocks(): List<Block> {
 }
 
 private fun List<Block>.checkSum(): Long = this.flatMap {
-    Block(if (it.id == -1) 0 else it.id, 1).repeat(it.size)
-}.mapIndexed { i, b -> i * b.id.toLong() }.sum()
+    Block(it.id ?: 0, 1).repeat(it.size)
+}.mapIndexed { i, b -> i * b.id!!.toLong() }.sum()
